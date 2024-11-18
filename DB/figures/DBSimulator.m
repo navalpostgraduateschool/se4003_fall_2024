3d8da6b293c662046f1565e18a1e021087650ebf
hanktron-dotcom <hankc498@gmail.com>
2024-11-18 14:45:27 -0800

    Create DBSimulator.m

---
+++ DB/figures/DBSimulator.m
@@ -0,0 +1,19 @@
+classdef duckBlindSimulator < handle
+    properties
+        swarmController   % Instance of the swarm controller (nested class)
+        laserController   % Instance of the laser controller
+        axis_handle       % Handle to the GUI axes for visualization
+    end
+    
+    methods
+        % Constructor
+        function this = duckBlindSimulator(laserController, axis_handle)
+            this.swarmController = duckBlindSimulator.swarmController(); % Create swarmController instance
+            this.laserController = laserController;
+            this.axis_handle = axis_handle;
+        end
+        
+        % Other methods (initializeSwarmController, run, etc.)
+        % Remain the same as previously provided
+    end
+end
