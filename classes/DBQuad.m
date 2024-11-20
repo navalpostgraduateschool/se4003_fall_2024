1b47c83e61778c08d5b6a5d09c1fc53074b0d6e1
jbahleda <joseph.bahleda@nps.edu>
2024-11-20 14:02:05 -0800

    More UAV Updates

--- classes/DBQuad.m
+++ classes/DBQuad.m
@@ -1,22 +1,22 @@
-classdef DBPredator < DBUAVA
-    % Predator UAV Class Inherits from DBUAVA
+classdef DBQuad < DBUAVA
+    % Quad UAV Class Inherits from DBUAVA
     
     properties (Constant)
-        MaxSpeed = 2.0; % Maximum speed for the Predator UAV
-        MarkerType = '^'; % Marker type for visualization
-        Color = [1, 0, 0]; % Color for visualization (red)
-        Size = 100; % Marker size for visualization
+        MaxSpeed = 1.5; % Maximum speed for the Quad UAV
+        MarkerType = 's'; % Marker type for visualization (square)
+        Color = [0, 0, 1]; % Color for visualization (blue)
+        Size = 80; % Marker size for visualization
         Icon = ''; % Optional: Icon (e.g., for UI representation)
     end
     
     methods
-        function obj = DBPredator(position, maxVelocity, armor)
+        function obj = DBQuad(position, maxVelocity, armor)
             % Call superclass constructor
             obj@DBUAVA(position, maxVelocity, armor);
         end
         
         function update(obj, dt, velocity)
-            % Update the position of the Predator UAV
+            % Update the position of the Quad UAV
             % Ensure velocity does not exceed MaxSpeed
             if norm(velocity) > obj.MaxSpeed
                 velocity = (velocity / norm(velocity)) * obj.MaxSpeed;
