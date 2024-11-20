8d6e747c84dbebc402b067d25bf778b22290b7e1
JakeDuckling <bryan.robbins@nps.edu>
2024-11-20 15:19:47 -0800

    Update initAxes.m

---
+++ auxiliary/initAxes.m
@@ -0,0 +1,18 @@
+function axesH = initAxes(~,axesH)
+            % Access the UIAxes component and update its properties
+            axesH.XLabel.String = '';  % Remove X label
+            axesH.YLabel.String = '';  % Remove Y label
+
+            % Remove ticks and tick labels
+            xticks(axesH, []);
+            yticks(axesH, []);
+
+            % Set the background color to black
+            axesH.Color = 'black';  % Set the background color of the axes to black
+
+            % Add a black border
+            axesH.Box = 'on';  % Turn on the box (border)
+            axesH.XColor = 'black';  % Set the color of the X axis line (black)
+            axesH.YColor = 'black';  % Set the color of the Y axis line (black)
+            axesH.LineWidth = 2;  % Optional: make the border thicker
+        end
