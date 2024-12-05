193ceb279e9c910d4c3a4033eca36294ce560300
Miron Lev <miron.lev.is@nps.edu>
2024-12-05 10:28:43 -0800

    Update DBLasedOnEvt.m

--- classes/DBLasedOnEvt.m
+++ classes/DBLasedOnEvt.m
@@ -8,13 +8,16 @@ classdef (ConstructOnLoad) DBLasedOnEvt < event.EventData
    end
 
    methods
-       function data = DBLasedOnEvt(droneObj, laserObj, duration_sec, varargin,inducedPower)
+       function data = DBLasedOnEvt(droneObj, laserObj, duration_sec,inducedPower,varargin)
           data.droneObj = droneObj;
           data.duration_sec = 1/20;
           if nargin>1
               data.laserObj = laserObj;
               if nargin>2
                   data.duration_sec = duration_sec;
+                  if nargin > 3
+                      data.inducedPower = inducedPower;
+                  end
               end
           end
           data.timeStamp = datetime('now');
